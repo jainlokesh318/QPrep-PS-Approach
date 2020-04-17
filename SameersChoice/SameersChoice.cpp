3bc5ec95ac78cac64a912b3db43e1e39a50fa16e
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-17 16:19:49 +0530

    Initial commit

---
+++ SameersChoice/SameersChoice.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include "solution.cpp"
+#include <cassert>
+
+
+void test_valid_numer_pattern()
+{
+    {
+        vehicle_info number1("TN-12-1234");
+        vehicle_info number2("TN-12-A-1234");
+        vehicle_info number3("TN-12-AB-1234");
+        vehicle_info number4("TN-12-ABC-1234");
+        vehicle_info number5("TN-12-ba-1234");
+
+        assert(true == number1.is_valid_number_pattern());
+        assert(true == number2.is_valid_number_pattern());
+        assert(true == number3.is_valid_number_pattern());
+        assert(true == number4.is_valid_number_pattern());
+        assert(true == number5.is_valid_number_pattern());
+
+        vehicle_info number6("TN-12-abcd-1234");
+        vehicle_info number7("TN-12-abcd-12-1234");
+        vehicle_info number8("TN-12-12345");
+        vehicle_info number9("TN-12-abcd");
+
+        assert(false == number6.is_valid_number_pattern());
+        assert(false == number7.is_valid_number_pattern());
+        assert(false == number8.is_valid_number_pattern());
+        assert(false == number9.is_valid_number_pattern());
+    }
+}
+
+void test_number_pattern()
+{
+    {
+        number_pattern pattern("31-23-45", "Car");
+
+        assert(pattern.number_matches_the_pattern("3145"));
+        assert(pattern.number_matches_the_pattern("3245"));
+        assert(pattern.number_matches_the_pattern("3135"));
+        assert(pattern.number_matches_the_pattern("3235"));
+
+        assert(!pattern.number_matches_the_pattern("2235"));
+        assert(!pattern.number_matches_the_pattern("3435"));
+        assert(!pattern.number_matches_the_pattern("3255"));
+        assert(!pattern.number_matches_the_pattern("3236"));
+    }
+
+    {
+        number_pattern pattern("31-23-45", "Car");
+        string expected("3135");
+        assert(expected == pattern.get_number_suggestion("4435"));
+    }
+
+    {
+        number_pattern pattern("31-23-45", "Car");
+        string expected("3145");
+        assert(expected == pattern.get_number_suggestion("4445"));
+    }
+}
+
+
+void test_palindrome()
+{
+    {
+        assert(true == is_palindrome("1221"));
+        assert(true == is_palindrome("121"));
+        assert(true == is_palindrome("11"));
+        assert(true == is_palindrome("1"));
+
+        assert(false == is_palindrome("1222"));
+        assert(false == is_palindrome("122"));
+        assert(false == is_palindrome("12"));
+    }
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    test_valid_numer_pattern();
+    test_number_pattern();
+    test_palindrome();
+
+    vector<number_pattern> patterns;
+    for (int i = 0; i < n; ++i) {
+        string pattern;
+        string type;
+
+        cin >> type >> pattern;
+
+        number_pattern one_pattern(pattern, type);
+        patterns.push_back(one_pattern);
+    }
+
+    int m;
+    cin >> m;
+    vector<vehicle_info> vehicles;
+    for (int i = 0; i < m; ++i) {
+        string type;
+        string number;
+
+        cin >> number >> type;
+
+        vehicle_info info(number, type);
+        vehicles.push_back(info);
+    }
+
+    lucky_number(vehicles, patterns);
+    return 0;
+}
+
---
+++ SameersChoice/SameersChoice.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+class SameersChoice {
+	void testValidNumberPattern() {
+		solution solve = new solution();
+		boolean number1 = solve.isValidNumberPlate("TN-12-1234");
+		boolean number2 = solve.isValidNumberPlate("TN-12-A-1234");
+		boolean number3 = solve.isValidNumberPlate("TN-12-AB-1234");
+		boolean number4 = solve.isValidNumberPlate("TN-12-ABC-1234");
+		
+		assert number1 == true;
+		assert number2 == true;
+		assert number3 == true;
+		assert number4 == true;
+
+		boolean number5 = solve.isValidNumberPlate("TN-12-ABCD-1234");
+		boolean number6 = solve.isValidNumberPlate("OLA1234");
+		boolean number7 = solve.isValidNumberPlate("TN-12-12345");
+		boolean number8 = solve.isValidNumberPlate("UT-12-abcd");
+		boolean number9 = solve.isValidNumberPlate("UT-12--4578");
+
+		assert number5 == false;
+		assert number6 == false;
+		assert number7 == false;
+		assert number8 == false;
+		assert number9 == false;
+
+	}	
+
+	void testNumberPattern() {
+		solution solve = new solution();
+		String number1[] = solve.isDesignatedAndFindMinimumByFourthPart("3145", "31-23-45");
+		String number2[] = solve.isDesignatedAndFindMinimumByFourthPart("3245", "31-23-45");
+		String number3[] = solve.isDesignatedAndFindMinimumByFourthPart("3135", "31-23-45");
+		String number4[] = solve.isDesignatedAndFindMinimumByFourthPart("3235", "31-23-45");
+
+		assert number1[0].equals("yes") == true;
+		assert number2[0].equals("yes") == true;
+		assert number3[0].equals("yes") == true;
+		assert number4[0].equals("yes") == true;
+
+		number1 = solve.isDesignatedAndFindMinimumByFourthPart("4435", "31-23-45");
+		number2 = solve.isDesignatedAndFindMinimumByFourthPart("4445", "31-23-45");
+		number3 = solve.isDesignatedAndFindMinimumByFourthPart("4415", "31-23-45");
+		String expected1 = "3135";
+		String expected2 = "3145";
+		String expected3 = "3135";
+
+		assert expected1.equals(number1[1]) == true;
+		assert expected2.equals(number2[1]) == true;
+		assert expected3.equals(number3[1]) == true;
+
+
+
+	}
+
+	void testPalindrome() {
+		solution solve = new solution();
+		assert solve.isPalindrome("1221") == true;
+		assert solve.isPalindrome("2112") == true;
+		assert solve.isPalindrome("1111") == true;
+		assert solve.isPalindrome("2121") == false;
+		assert solve.isPalindrome("2221") == false;
+		assert solve.isPalindrome("2212") == false;
+	}
+
+	public static void main(String args[]) {
+		
+		SameersChoice sameer = new  SameersChoice();
+
+		sameer.testValidNumberPattern();
+		sameer.testNumberPattern();
+		sameer.testPalindrome();
+
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> finalPlates = new ArrayList<Integer>();
+		int noOfPatterns = scanner.nextInt();
+		String vehicleType[] = new String[noOfPatterns];
+		String regPattern[] = new String[noOfPatterns];
+		for(int i  = 0 ; i < noOfPatterns ; i++) {
+			vehicleType[i] = scanner.next();
+			regPattern[i] = scanner.next();
+		}
+		int noOfPlates = scanner.nextInt();
+		String plate[] = new String[noOfPlates];
+		String vehicle[] = new String[noOfPlates];
+		for(int i = 0 ; i < noOfPlates ; i++) {
+			plate[i] = scanner.next();
+			vehicle[i] = scanner.next();
+		}
+		solution solve = new solution();
+		solve.luckyNumber(noOfPatterns,noOfPlates,vehicleType,regPattern,plate,vehicle);
+	}
+
+}
+
