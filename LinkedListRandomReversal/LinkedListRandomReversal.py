a5456e4608430b8d9e7001527e3baab071472afb
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-17 17:18:53 +0550

    updated changes - 2020-04-17 16:58:53

--- LinkedListRandomReversal/LinkedListRandomReversal.py
+++ LinkedListRandomReversal/LinkedListRandomReversal.py
@@ -4,57 +4,6 @@ from solution import *
 
 
 
-def test_append_reversed_list_to_result():
-    result_head = None
-    result_tail = None
-
-
-    res = Solution().append_reversed_list_to_result(result_head, result_tail, None, None)
-    result_head = res[0]
-    result_tail = res[1]
-    assert result_head == None
-    assert result_tail == None
-
-    arr1 = [1, 2, 3]
-
-    list1 = createList(arr1)
-
-    res = Solution().append_reversed_list_to_result(result_head, result_tail, list1, list1.next.next)
-    result_head = res[0]
-    result_tail = res[1]
-
-    expected_arr1 = extractList(result_head)
-    # now the lsit should look like 1->2->3->None
-    assert expected_arr1[0] == arr1[0]
-    assert expected_arr1[1] == arr1[1]
-    assert expected_arr1[2] == arr1[2]
-
-    arr2 = [4, 5]
-
-    list2 = createList(arr2)
-    res = Solution().append_reversed_list_to_result(result_head, result_tail, list2, list2.next)
-    result_head = res[0]
-    result_tail = res[1]
-
-    expected_arr2 = extractList(result_head)
-    # now the lsit should look like 1->2->3->4->5->None
-    assert expected_arr1[0] == arr1[0]
-    assert expected_arr1[1] == arr1[1]
-    assert expected_arr1[2] == arr1[2]
-    assert expected_arr2[3] == arr2[0]
-    assert expected_arr2[4] == arr2[1]
-
-    res = Solution().append_reversed_list_to_result(result_head, result_tail, None, None)
-    result_head = res[0]
-    result_tail = res[1]
-    # now the lsit should look like 1->2->3->4->5->None
-    assert expected_arr1[0] == arr1[0]
-    assert expected_arr1[1] == arr1[1]
-    assert expected_arr1[2] == arr1[2]
-    assert expected_arr2[3] == arr2[0]
-    assert expected_arr2[4] == arr2[1]
-
-
 n = int(sys.stdin.readline())
 lst = list(map(int , sys.stdin.readline().strip().split()))
 
