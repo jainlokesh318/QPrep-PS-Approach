2e13db3726bf8a737ff8a3c1686f64722d32cfb2
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-17 16:19:47 +0530

    Initial commit

---
+++ SafeSquare/SafeSquare.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "solution.cpp"
+
+int main()
+{
+
+    int rows,cols;
+    vector<vector<char>> board;
+	cin >> rows >> cols;
+	board.resize(rows,vector<char>(cols));
+	for(int i = 0 ; i< rows ; i++) {
+		for(int j = 0 ;j < cols ; j++) {
+			cin >> board[i][j];
+		}
+	}
+    chess chess_board(board);
+
+	cout << chess_board.move() << endl;
+	return 0;
+}
+
---
+++ SafeSquare/SafeSquare.py
@@ -0,0 +1,18 @@
+from solution import *
+
+
+def main():
+
+
+    (n, m) = [int(x) for x in input().split()]
+    mat = [input().strip() for i in range(n)]
+
+    board = chess(mat)
+
+    print(board.move())
+    #print_mat(mat)
+    #print_mat(board.result)
+
+
+if __name__ == '__main__':
+    main()
