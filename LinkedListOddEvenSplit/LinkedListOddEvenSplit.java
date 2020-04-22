64c5230ac52be367e3f077e4743cc615dc26be49
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-22 16:51:10 +0550

    updated changes - 2020-04-22 16:31:10

--- LinkedListOddEvenSplit/LinkedListOddEvenSplit.java
+++ LinkedListOddEvenSplit/LinkedListOddEvenSplit.java
@@ -6,6 +6,30 @@ import crio.ds.List.ListNode;
 
 
 class LinkedListOddEvenSplit {
+
+    private static ListNode createListWithLoop(List<Integer> numbers, int k) {
+        if(k == -1) {
+            return ListNode.createList(numbers);
+        }
+        ListNode head = new ListNode(numbers.get(0));
+        ListNode current = head;
+        ListNode kth_node = null;
+
+        --k;
+        if(k == 0) {
+            kth_node = current;
+        }
+        for(int i = 1 ; i < numbers.size() ; ++i) {
+            current.next = new ListNode(numbers.get(i));
+            current = current.next;
+            --k;
+            if(k == 0) {
+                kth_node = current;
+            }
+        }
+        current.next = kth_node;
+        return head;
+    }
     private static void print_list(ListNode head) {
         List<Integer> list = ListNode.extractList(head);
         System.out.println(list.size());
