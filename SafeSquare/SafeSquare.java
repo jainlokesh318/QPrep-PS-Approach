ac02e88bd4ab37e0246bf69806c26c2e36eed508
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-22 21:44:57 +0550

    updated changes - 2020-04-22 21:24:57

---
+++ SafeSquare/SafeSquare.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.Math;
+
+// DON'T TOUCH THIS FILE. ASSESSMENT WILL REPLACE THIS FILE
+
+class SafeSquare{
+
+    public static void main(String[] args) {
+
+		solution so=new solution();
+		so.readInput();
+		int ans=so.move();
+		System.out.println(ans);
+	}
+}
+
