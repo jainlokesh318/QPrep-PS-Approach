4416106aa5485dad975de51027ed52907a43bc77
Arun prasath Nagarajan <jainlokesh318@gmail.com>
2020-04-24 23:12:20 +0550

    updated changes - 2020-04-24 22:52:20

--- SameersChoice/solution.java
+++ SameersChoice/solution.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-class SameersChoice {
+
 class solution {
 	void luckyNumber(int noOfPatterns,int noOfPlates,String vehicleType[],String regPattern[],String plate[],String vehicle[]) {
 	}
